638b8db9cb484042aeeb581785efd7aa290f5118
Dmitriy Gavrilov <d_man_sun@mail.ru>
2018-05-15 00:28:12 +0600

    Mobile menu. Basic

--- src/js/app.js
+++ src/js/app.js
@@ -17,4 +17,51 @@ $( document ).ready(function() {
     return false;
   });
 
+  /** Mobile menu button */
+
+  const headerMobileMenuToggleButton = $( "#header-mobile-menu-btn" );
+  const headerMobileMenu = $('#header-mobile-menu');
+
+  (function ($) {
+
+    headerMobileMenuToggleButton.on( 'click', function () {
+
+      const $this = $( this );
+      const activeClass = 'is-active';
+
+      if( headerMobileMenu.css('display') === 'none' ) {
+        headerMobileMenu.css({ 'display' : 'block' });
+        $this.addClass( activeClass );
+      } else {
+        headerMobileMenu.css({ 'display' : 'none' });
+        $this.removeClass( activeClass );
+      }
+
+    });
+
+  })($);
+
+  /** Set mobile menu width */
+
+  (function ($) {
+
+    setMobileMenuWidth();
+
+    $( window ).resize(function() {
+      setMobileMenuWidth();
+    });
+
+    function setMobileMenuWidth() {
+
+      const rightOffsetFromWindow = ($(window).width() - (headerMobileMenuToggleButton.offset().left + headerMobileMenuToggleButton.outerWidth()));
+      const rightPadding = 10;
+      const offset = rightOffsetFromWindow + rightPadding;
+      const width = 'calc(100% - ' + offset + 'px)';
+
+      headerMobileMenu.css({'width' : width});
+
+    }
+
+  })($);
+
 });
